0c00aa18d8b538b0f980406646aa0f97ca546380
Mikkel <mikkelhansen.92@jubii.dk>
2017-06-06 23:18:19 +0200

    Påbegyndt login

---
+++ SimpleRESTServer/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleRESTServer.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET: api/Login
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Login/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Used by visitors to login.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        // POST: api/Login
+        public void Post([FromBody]string username, [FromBody]string password)
+        {
+            LoginPersistence lp = new LoginPersistence();
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SimpleRESTServer/LoginPersistence.cs
@@ -0,0 +1,61 @@
+﻿using SimpleRESTServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleRESTServer
+{
+    public class LoginPersistence
+    {
+        string myConnectionString = "server=databasetest.cgredk49egoe.eu-central-1.rds.amazonaws.com;uid=Mikkel;pwd=aln1poin;database=LalaDB";
+
+        public User Login(string username, string password)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn = null;
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                string sqlString = "SELECT * FROM users WHERE username='" + username + "'";
+
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+                MySql.Data.MySqlClient.MySqlDataReader mySQLReader = cmd.ExecuteReader();
+
+                User user = new User();
+
+                if (mySQLReader.Read())
+                {
+                    user.Id = mySQLReader.GetInt32(0);
+                    user.UserName = mySQLReader.GetString(1);
+                    user.Password = mySQLReader.GetString(2);
+                    user.FirstName = mySQLReader.GetString(3);
+                    user.LastName = mySQLReader.GetString(4);
+                    user.Token = mySQLReader.GetString(5);
+                }
+
+                if(user.Password.Equals(password) && user.UserName.Equals(username))
+                {
+                    return user;
+                } else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
