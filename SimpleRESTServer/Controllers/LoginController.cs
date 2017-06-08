e4c624e10904948503b6c44e855908f9611869b1
Mikkel <mikkelhansen.92@jubii.dk>
2017-06-08 13:20:33 +0200

    Fået sat det op til laptop og fikset login.

--- SimpleRESTServer/Controllers/LoginController.cs
+++ SimpleRESTServer/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleRESTServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,12 +10,21 @@ namespace SimpleRESTServer.Controllers
 {
     public class LoginController : ApiController
     {
-        // GET: api/Login
+        /// <summary>
+        /// Not used
+        /// </summary>
+        /// <returns></returns>
+        //GET: api/Login
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        /// <summary>
+        /// Not used
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET: api/Login/5
         public string Get(int id)
         {
@@ -24,20 +34,37 @@ namespace SimpleRESTServer.Controllers
         /// <summary>
         /// Used by visitors to login.
         /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
+        /// <param name="login"></param>
         // POST: api/Login
-        public void Post([FromBody]string username, [FromBody]string password)
+        [HttpPost]
+        public User Post(Login login)
         {
             LoginPersistence lp = new LoginPersistence();
-        }
+            User userLoggedIn = lp.Login(login.UserName, login.Password);
 
+            if (userLoggedIn != null)
+            {
+                return userLoggedIn;
+            } else
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+            }
+        }
+        /// <summary>
+        /// Not used
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         // PUT: api/Login/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/Login/5
+        /// <summary>
+        /// Not used
+        /// </summary>
+        /// <param name="id"></param>
+        //// DELETE: api/Login/5
         public void Delete(int id)
         {
         }
